a0b07ff0b2c76201383abcc3cb21a3d764b45f32
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 08:27:30 +0100

    [ADD] Button to force marge negative

---
+++ hodei_force_marge/models/sale_order.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    force_marge = fields.Boolean('Force marge', default=False)
+
+    def create(self, vals):
+        if vals['marge_negative'] == True:
+            if vals['force_marge'] == True:
+                return super(SaleOrder, self).create(vals)
+            else:
+                raise ValidationError("Some line have negative marge. Fix it or check the force marge button")
