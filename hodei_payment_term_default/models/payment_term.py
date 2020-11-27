b7e7fd75d6e90de78149581d9ed6c7a9727ed8eb
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 09:09:51 +0100

    [WIP]

--- hodei_payment_term_default/models/payment_term.py
+++ hodei_payment_term_default/models/payment_term.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import UserError
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -21,12 +21,12 @@ _logger = logging.getLogger(__name__)
             else:
                 self.default_term = True
 
-        def write(self, vals):
-            if vals['default_term']:
-                if vals['default_term'] == True:
-                # Check if an other default_term is set
-                already_set = self.env['account.payment.term'].search([('default_term', '=', True)])
-                if already_set:
-                    raise UserError(_('An other payment term is set to be the default one : %s') % already_set)
-                    vals['default_term'] = False
-        return super(AccountPaymentTerm, self).write(vals)
+        # def write(self, vals):
+        #     if vals['default_term']:
+        #         if vals['default_term'] == True:
+        #         # Check if an other default_term is set
+        #         already_set = self.env['account.payment.term'].search([('default_term', '=', True)])
+        #         if already_set:
+        #             raise UserError(_('An other payment term is set to be the default one : %s') % already_set)
+        #             vals['default_term'] = False
+        # return super(AccountPaymentTerm, self).write(vals)
