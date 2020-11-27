4a86a5b8337705c85e3b9641af5b912b1eb041f7
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 09:43:56 +0100

    [WIP]

--- hodei_payment_term_default/models/res_partner.py
+++ hodei_payment_term_default/models/res_partner.py
@@ -5,10 +5,8 @@ from odoo import api, fields, models, _
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    def _default_payment_term(self):
-        return self.env['account.payment.term'].search([('default_term', '=', True)])
 
     property_payment_term_id = fields.Many2one('account.payment.term', company_dependent=True,
         string='Customer Payment Terms',
         help="This payment term will be used instead of the default one for sales orders and customer invoices", 
-        oldname="property_payment_term", default=lambda self: self._default_payment_term())
+        oldname="property_payment_term", default=lambda self: self.env['account.payment.term'].search([('default_term', '=', True)]))
