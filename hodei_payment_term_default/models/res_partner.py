ec992890f52073fa60d7b554e6f5676b6bc12e57
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 09:49:12 +0100

    [WIP]

--- hodei_payment_term_default/models/res_partner.py
+++ hodei_payment_term_default/models/res_partner.py
@@ -9,4 +9,4 @@ class ResPartner(models.Model):
     property_payment_term_id = fields.Many2one('account.payment.term', company_dependent=True,
         string='Customer Payment Terms',
         help="This payment term will be used instead of the default one for sales orders and customer invoices", 
-        oldname="property_payment_term", default=lambda self: self.env['account.payment.term'].search([('default_term', '=', True)]))
+        oldname="property_payment_term", default=lambda self: self.env['account.payment.term'].search([('default_term', '=', True)])[0])
