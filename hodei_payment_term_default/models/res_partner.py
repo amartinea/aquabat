49cc83aa36c9ea92d542e0a758157e70eb137e0e
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 11:57:50 +0100

    [FIX] Add it on create function not write one

--- hodei_payment_term_default/models/res_partner.py
+++ hodei_payment_term_default/models/res_partner.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-
-    def write(self, vals):
+    @api.model
+    def create(self, vals):
         payment_default = self.env['account.payment.term'].search([('default_term', '=', True)], limit=1)
+        _logger.warning("-----------------------")
+        _logger.warning(vals)
         if not 'property_payment_term_id' in vals and payment_default:
             vals['property_payment_term_id'] = payment_default
-        return super(ResPartner, self).write(vals)
+        return super(ResPartner, self).create(vals)
