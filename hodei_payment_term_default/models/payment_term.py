3d21fe8162d2968f06daa895772acb744a1ee22f
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 08:52:46 +0100

    [FIX] Warning name

--- hodei_payment_term_default/models/payment_term.py
+++ hodei_payment_term_default/models/payment_term.py
@@ -7,29 +7,29 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class AccountPaymentTerm(models.Model):
-    _inherit = "account.payment.term"
+	class AccountPaymentTerm(models.Model):
+	    _inherit = "account.payment.term"
 
-    default_term = fields.Boolean('Default term', default=False)
+	    default_term = fields.Boolean('Default term', default=False)
 
-    @api.onchange('default_term')
-    def on_default_term(self):
-        # Check if an other default_term is set
-        already_set = self.env['account.payment.term'].search([('default_term', '=', True)])
-        _logger.warning("--------------------")
-        _logger.warning(already_set)
-        _logger.waring(self)
-        if already_set:
-            raise UserError(_('An other payment term is set to be the default one : %s') % already_set)
-        else:
-            self.default_term = True
+	    @api.onchange('default_term')
+	    def on_default_term(self):
+	        # Check if an other default_term is set
+	        already_set = self.env['account.payment.term'].search([('default_term', '=', True)])
+	        _logger.warning("--------------------")
+	        _logger.warning(already_set)
+	        _logger.warning(self)
+	        if already_set:
+	            raise UserError(_('An other payment term is set to be the default one : %s') % already_set)
+	        else:
+	            self.default_term = True
 
-    def write(self, vals):
-        _logger.warning("--------------------")
-        _logger.warning(vals['default_term'])
-        _logger.waring(self['default_term'])
-        if vals['default_term']:
-            if vals['default_term'] == True:
+	    def write(self, vals):
+	        _logger.warning("--------------------")
+	        _logger.warning(vals['default_term'])
+	        _logger.warning(self['default_term'])
+	        if vals['default_term']:
+	            if vals['default_term'] == True:
                 # Check if an other default_term is set
                 already_set = self.env['account.payment.term'].search([('default_term', '=', True)])
                 if already_set:
