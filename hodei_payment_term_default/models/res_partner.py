fe8ff361b00ce05bc06887a24f0c6737fe851f77
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 15:31:21 +0100

    [FIX] Case with contact create from partner

--- hodei_payment_term_default/models/res_partner.py
+++ hodei_payment_term_default/models/res_partner.py
@@ -9,6 +9,6 @@ class ResPartner(models.Model):
     @api.model
     def create(self, vals):
         payment_default = self.env['account.payment.term'].search([('default_term', '=', True)], limit=1)
-        if vals['property_payment_term_id'] == False and payment_default:
+        if 'property_payment_term_id' in vals and vals['property_payment_term_id'] == False and payment_default:
             vals['property_payment_term_id'] = payment_default
         return super(ResPartner, self).create(vals)
