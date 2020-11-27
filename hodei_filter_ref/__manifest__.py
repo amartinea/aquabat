2b6cb74bad9ddae1d9f21a8de55e0c884a688241
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 07:35:19 +0100

    [ADD] DEV1.1.4 Ref client filter module

---
+++ hodei_filter_ref/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hodei Filter Customer Ref module',
+    'version': '1.0.0',
+    'category': '',
+    'description': """ """,
+    'depends': [
+        'sale',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'views/sale_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
