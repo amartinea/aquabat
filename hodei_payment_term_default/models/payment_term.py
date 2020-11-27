2ff74427ec555986c3b4e728a08d8ab87905fe2d
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-27 09:11:37 +0100

    [WIP] Log

--- hodei_payment_term_default/models/payment_term.py
+++ hodei_payment_term_default/models/payment_term.py
@@ -7,26 +7,29 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-    class AccountPaymentTerm(models.Model):
-        _inherit = "account.payment.term"
+class AccountPaymentTerm(models.Model):
+    _inherit = "account.payment.term"
 
-        default_term = fields.Boolean('Default term', default=False)
+    default_term = fields.Boolean('Default term', default=False)
 
-        @api.onchange('default_term')
-        def on_default_term(self):
+    @api.onchange('default_term')
+    def on_default_term(self):
+        # Check if an other default_term is set
+        already_set = self.env['account.payment.term'].search([('default_term', '=', True)])
+        _logger.warning("-------------------")
+        _logger.warning(self)
+        _logger.warning(already_set)
+        if already_set:
+            raise UserError(_('An other payment term is set to be the default one : %s') % already_set)
+        else:
+            self.default_term = True
+
+    def write(self, vals):
+        if vals['default_term']:
+            if vals['default_term'] == True:
             # Check if an other default_term is set
             already_set = self.env['account.payment.term'].search([('default_term', '=', True)])
             if already_set:
                 raise UserError(_('An other payment term is set to be the default one : %s') % already_set)
-            else:
-                self.default_term = True
-
-        # def write(self, vals):
-        #     if vals['default_term']:
-        #         if vals['default_term'] == True:
-        #         # Check if an other default_term is set
-        #         already_set = self.env['account.payment.term'].search([('default_term', '=', True)])
-        #         if already_set:
-        #             raise UserError(_('An other payment term is set to be the default one : %s') % already_set)
-        #             vals['default_term'] = False
-        # return super(AccountPaymentTerm, self).write(vals)
+                vals['default_term'] = False
+    return super(AccountPaymentTerm, self).write(vals)
