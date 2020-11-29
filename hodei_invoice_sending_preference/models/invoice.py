2ffad03aa3e9142ff29a33e19747238c0d00dfa4
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-29 14:24:27 +0100

    [IMP] DEV1.1.7 Trigger it too for invoice from order

--- hodei_invoice_sending_preference/models/invoice.py
+++ hodei_invoice_sending_preference/models/invoice.py
@@ -18,3 +18,21 @@ class AccountInvoice(models.Model):
             self.sending_preference = self.partner_id.parent_id.sending_preference
         else:
             self.sending_preference = 'none'
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+
+    @api.multi
+    def _create_invoice(self, order, so_line, amount):
+        invoice = super(SaleAdvancePaymentInv, self)._create_invoice(order, so_line, amount)
+        if order:
+            if order.partner_id.sending_preference != 'none':
+                sending_preference = order.partner_id.sending_preference
+            elif order.partner_id.parent_id:
+                sending_preference = order.partner_id.parent_id.sending_preference
+            else:
+                sending_preference = 'none'
+            invoice.write({'sending_preference': sending_preference})
+            invoice.write({'customer_name': order['customer_name']})
+        return invoice
