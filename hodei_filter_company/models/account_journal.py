413a13fb8773f7d2deb4fb03af96a287918328cf
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-11-30 15:39:27 +0100

    [WIP] Logs

--- hodei_filter_company/models/account_journal.py
+++ hodei_filter_company/models/account_journal.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+import logging
+_logger = logging.getLogger(__name__)
+
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
@@ -10,4 +13,6 @@ class AccountJournal(models.Model):
 
         action = super(AccountJournal, self).open_action()
         action.update({'context': (action.get('context') or '').update({'search_default_company': 1})})
+        _logger.warning("--------------------------------------")
+        _logger.warning(action)
         return action
