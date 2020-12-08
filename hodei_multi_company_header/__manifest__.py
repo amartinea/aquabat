08adc10a5f5daa231a2936a0649f5fd099a7a7c0
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-08 16:01:53 +0100

    [ADD] Module to display custom header for each company

---
+++ hodei_multi_company_header/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hodei custom header',
+    'version': '1.0.0',
+    'category': '',
+    'description': """ """,
+    'depends': [
+        'web',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'views/header_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
