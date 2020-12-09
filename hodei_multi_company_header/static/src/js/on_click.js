08e66c0103704f1f77f38542042a365fd1eb2e53
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-09 08:45:14 +0100

    [WIP]

---
+++ hodei_multi_company_header/static/src/js/on_click.js
@@ -0,0 +1,42 @@
+odoo.define('web.SwitchCompanyMenu', function(require) {
+"use strict";
+
+/**
+ * When Odoo is configured in multi-company mode, users should obviously be able
+ * to switch their interface from one company to the other.  This is the purpose
+ * of this widget, by displaying a dropdown menu in the systray.
+ */
+
+var config = require('web.config');
+var core = require('web.core');
+var session = require('web.session');
+var SystrayMenu = require('web.SystrayMenu');
+var SwitchCompanyMenu = require('web.SwitchCompanyMenu');
+
+var _t = core._t;
+
+SwitchCompanyMenu.include({
+    _onClick: function (ev) {
+        ev.preventDefault();
+        var companyID = $(ev.currentTarget).data('company-id');
+        if (companyID == 1)
+        {
+            ev.currentTarget.parentElement.parentElement.parentElement.parentElement.find('.header-company-marsan').removeClass('header-company-marsan').addClass('header-company-negoce');
+        }
+        else if (companyID == 3)
+        {
+            ev.currentTarget.parentElement.parentElement.parentElement.parentElement.find('.header-company-negoce').removeClass('header-company-negoce').addClass('header-company-marsan');
+        }
+        this._rpc({
+            model: 'res.users',
+            method: 'write',
+            args: [[session.uid], {'company_id': companyID}],
+        })
+        .then(function() {
+            location.reload();
+        });
+    },
+});
+
+SystrayMenu.Items.push(SwitchCompanyMenu);
+return SwitchCompanyMenu
--- hodei_multi_company_header/static/src/js/qweb_template.js
+++
@@ -1,7 +0,0 @@
-odoo.define('hodei_multi_company_header.inherit_header', function(require) {
-'use strict';
-var core = require('web.core');
-var ajax = require('web.ajax');
-var qweb = core.qweb;
-ajax.loadXML('/hodei_multi_company_header/static/src/xml/header_view.xml', qweb);
-});
