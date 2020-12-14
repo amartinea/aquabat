9327e153bbf822fa8a748d28b65c2341f79893ca
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-14 15:43:56 +0100

    [WIP]

--- hodei_available_stock/models/sale_order.py
+++ hodei_available_stock/models/sale_order.py
@@ -7,14 +7,14 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    available_stock = fields.Selection([('Green', 'Green'), ('Red', 'Red')], string='Available Stock', compute="_compute_available_stock", store=True)
+    available_stock = fields.Boolean('Available Stock', compute="_compute_available_stock")
 
     def _compute_available_stock(self):
         for line in self:
             try:
                 if line.product_id.qty_real_available > line.product_qty:
-                    line.available_stock = 'Green'
+                    line.available_stock = True
                 else:
-                    line.available_stock = 'Red'
+                    line.available_stock = False
             except:
-                line.available_stock = 'Red'
+                line.available_stock = False
