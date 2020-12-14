581509c11c74dea2a84261bb1730690790b53cc5
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-14 11:18:46 +0100

    [FIX] Field already exist

--- hodei_unit_price_discounted/models/sale_order.py
+++ hodei_unit_price_discounted/models/sale_order.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# # -*- coding: utf-8 -*-
+# # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+# from odoo import api, fields, models
 
 
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
+# class SaleOrderLine(models.Model):
+#     _inherit = "sale.order.line"
 
-    unit_price_net = fields.Float('Unit price net', compute="_compute_unit_price_net", store=True)
+#     unit_price_net = fields.Float('Unit price net', compute="_compute_unit_price_net", store=True)
 
-    @api.depends('price_unit', 'price_reduce_taxexcl')
-    def _compute_unit_price_net(self):
-        for line in self:
-            line.unit_price_net = line.price_unit - line.price_reduce_taxexcl
+#     @api.depends('price_unit', 'price_reduce_taxexcl')
+#     def _compute_unit_price_net(self):
+#         for line in self:
+#             line.unit_price_net = line.price_unit - line.price_reduce_taxexcl
