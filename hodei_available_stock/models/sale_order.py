b9b2b784dd879e122ebf6cebe3b4b6b414561b6f
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-14 14:50:04 +0100

    [FIX] Use color widget

--- hodei_available_stock/models/sale_order.py
+++ hodei_available_stock/models/sale_order.py
@@ -7,14 +7,14 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    available_stock = fields.Boolean('Available Stock', compute="_compute_available_stock", store=True)
+    available_stock = fields.Char('Available Stock', compute="_compute_available_stock", store=True)
 
     def _compute_available_stock(self):
         for line in self:
             try:
                 if line.product_id.qty_real_available > line.product_qty:
-                    line.available_stock = True
+                    line.available_stock = 'green'
                 else:
-                    line.available_stock = False
+                    line.available_stock = 'red'
             except:
-                line.available_stock = False
+                line.available_stock = 'red'
