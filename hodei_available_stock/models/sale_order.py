4801b71a46813b877a8dd3d546de493cc252adb2
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-14 19:15:45 +0100

    [FIX] Give the id and not the object

--- hodei_available_stock/models/sale_order.py
+++ hodei_available_stock/models/sale_order.py
@@ -14,10 +14,10 @@ class SaleOrderLine(models.Model):
     def _compute_available_stock(self):
         for line in self:
             line.available_stock_global = line.product_id.qty_real_available
-            line.available_stock_company = line.product_id._compute_quantities_dict_by_company(line.order_id.company_id)
+            line.available_stock_company = line.product_id._compute_quantities_dict_by_company(line.order_id.company_id.id)
             try:
                 if line.order_id.company_id:
-                    if line.product_id._compute_quantities_dict_by_company(line.order_id.company_id) > line.product_qty:
+                    if line.product_id._compute_quantities_dict_by_company(line.order_id.company_id.id) > line.product_qty:
                         line.available_stock = True
                     else:
                         line.available_stock = False
