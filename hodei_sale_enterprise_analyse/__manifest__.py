f92ba3a46b8c780b44879e94a6f1005413c3f39a
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-14 17:22:18 +0100

    [ADD] Module to add information on sale anylse view

---
+++ hodei_sale_enterprise_analyse/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hodei sale enterprise analyse module',
+    'version': '1.0.0',
+    'category': '',
+    'description': """ """,
+    'depends': [
+        'sale_enterprise',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+       'report/sale_report.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
