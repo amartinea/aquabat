89b3237ca17fe8436f5864e0bdf4d29094490bb2
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-18 09:01:34 +0100

    [ADD] Defaut route after chose a company

--- hodei_default_route/models/sale_order.py
+++ hodei_default_route/models/sale_order.py
@@ -10,4 +10,10 @@ class SaleOrderLine(models.Model):
     def _default_route(self):
         return self.env['stock.location.route'].search([('default_route', '=', True), ('company_id', '=', self._context.get('order_id').get('company_id'))], limit=1)
 
-    route_id = fields.Many2one(default=_default_route)
+
+    @api.onchange('company_id')
+    def on_company_id(self):
+        # Check if an other default_route is set for this company
+        if self.company_id:
+            self.route_id = self.env['stock.location.route'].search([('default_route', '=', True), ('company_id', '=', self.company_id)])
+
