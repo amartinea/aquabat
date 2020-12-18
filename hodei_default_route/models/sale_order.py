c731acab65f0de2d0fe909dd6f4eca61e42770d5
Arthur Martineau <arthur.martineau@meta-it.fr>
2020-12-18 11:45:37 +0100

    [FIX] No alternative update at this moment

--- hodei_default_route/models/sale_order.py
+++ hodei_default_route/models/sale_order.py
@@ -6,7 +6,6 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-
     route_id = fields.Many2one(compute='_compute_company_id')
 
     @api.multi
@@ -15,3 +14,13 @@ class SaleOrderLine(models.Model):
         for line in self:
             if line.order_id.company_id:
                 line.route_id = self.env['stock.location.route'].search([('default_route', '=', True), ('company_id', '=', line.order_id.company_id.id)])
+
+# class SaleOrder(models.Model):
+#     _inherit = "sale.order"
+
+#     def _use_alternative_route(self):
+#         for order in self:
+#             if order.company_id:
+#                 for line in order.order_line: 
+#                     line.route_id = self.env['stock.location.route'].search([('default_update_route', '=', True), ('company_id', '=', line.order_id.company_id.id)])
+
--- hodei_default_route/models/stock_route.py
+++ hodei_default_route/models/stock_route.py
@@ -8,7 +8,7 @@ class StockRoute(models.Model):
     _inherit = "stock.location.route"
 
     default_route = fields.Boolean(string="Route par défaut")
-    default_update_route = fields.Boolean(string="Seconde route par défaut")
+    # default_update_route = fields.Boolean(string="Seconde route par défaut")
 
     @api.onchange('default_route')
     def on_default_route(self):
@@ -20,15 +20,15 @@ class StockRoute(models.Model):
             else:
                 self.default_route = True
 
-    @api.onchange('default_update_route')
-    def on_default_update_route(self):
-        # Check if an other default_update_route is set for this company
-        if self['default_update_route']:
-            already_set = self.env['stock.location.route'].search([('default_update_route', '=', True), ('company_id', '=', self['company_id'].id)])
-            if already_set:
-                raise UserError(_('An other second defaut route is set to be the default one : %s') % already_set.name)
-            else:
-                self.default_update_route = True
+    # @api.onchange('default_update_route')
+    # def on_default_update_route(self):
+    #     # Check if an other default_update_route is set for this company
+    #     if self['default_update_route']:
+    #         already_set = self.env['stock.location.route'].search([('default_update_route', '=', True), ('company_id', '=', self['company_id'].id)])
+    #         if already_set:
+    #             raise UserError(_('An other second defaut route is set to be the default one : %s') % already_set.name)
+    #         else:
+    #             self.default_update_route = True
 
     def write(self, vals):
         if 'default_route' in vals and vals['default_route']:
@@ -38,11 +38,11 @@ class StockRoute(models.Model):
                 if already_set_default:
                     raise UserError(_('An other default route is set to be the default one : %s') % already_set_default.name)
                     vals['default_route'] = False
-        if 'default_update_route' in vals and vals['default_update_route']:
-            if vals['default_update_route'] == True:
-                # Check if an other default_update_route is set
-                already_set_update_default = self.env['stock.location.route'].search([('default_update_route', '=', True), ('company_id', '=', self['company_id'].id)])
-                if already_set_update_default:
-                    raise UserError(_('An other second default route is set to be the default one : %s') % already_set_update_default.name)
-                    vals['default_update_route'] = False
+        # if 'default_update_route' in vals and vals['default_update_route']:
+        #     if vals['default_update_route'] == True:
+        #         # Check if an other default_update_route is set
+        #         already_set_update_default = self.env['stock.location.route'].search([('default_update_route', '=', True), ('company_id', '=', self['company_id'].id)])
+        #         if already_set_update_default:
+        #             raise UserError(_('An other second default route is set to be the default one : %s') % already_set_update_default.name)
+        #             vals['default_update_route'] = False
         return super(StockRoute, self).write(vals)
