9ae097252f30b9b2ca49875c34a146ece9fa15a5
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-01-04 10:24:10 +0100

    [WIP]

--- hodei_default_route/models/sale_order.py
+++ hodei_default_route/models/sale_order.py
@@ -8,13 +8,17 @@ class SaleOrderLine(models.Model):
 
     route_id = fields.Many2one(readonly=False)
 
-    @api.multi
-    @api.depends('order_id.company_id')
-    def _on_change_company_id(self):
-        for line in self:
-            if line.order_id.company_id:
-                line.route_id = self.env['stock.location.route'].search([('default_route', '=', True), ('company_id', '=', line.order_id.company_id.id)])
+    # @api.multi
+    # @api.depends('order_id.company_id')
+    # def _on_change_company_id(self):
+    #     for line in self:
+    #         if line.order_id.company_id:
+    #             line.route_id = self.env['stock.location.route'].search([('default_route', '=', True), ('company_id', '=', line.order_id.company_id.id)])
 
+    def write(self, vals):
+    	if self.order_id.company_id:
+            vals['route_id'] = self.env['stock.location.route'].search([('default_route', '=', True), ('company_id', '=', self.order_id.company_id.id)])
+    	return super(StockRoute, self).write(vals)
 # class SaleOrder(models.Model):
 #     _inherit = "sale.order"
 
