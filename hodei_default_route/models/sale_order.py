c8f7df8bfb346a10adabaf5e8fb8ee9e348f19ae
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-01-04 10:25:10 +0100

    [FIX] Indent

--- hodei_default_route/models/sale_order.py
+++ hodei_default_route/models/sale_order.py
@@ -16,9 +16,9 @@ class SaleOrderLine(models.Model):
     #             line.route_id = self.env['stock.location.route'].search([('default_route', '=', True), ('company_id', '=', line.order_id.company_id.id)])
 
     def write(self, vals):
-    	if self.order_id.company_id:
+        if self.order_id.company_id:
             vals['route_id'] = self.env['stock.location.route'].search([('default_route', '=', True), ('company_id', '=', self.order_id.company_id.id)])
-    	return super(StockRoute, self).write(vals)
+        return super(StockRoute, self).write(vals)
 # class SaleOrder(models.Model):
 #     _inherit = "sale.order"
 
