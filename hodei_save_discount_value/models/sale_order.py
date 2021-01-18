c7c6ff3ca819c919f38fd4ed0c8054b96e162d5b
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-01-18 14:33:08 +0100

    [WIP]

--- hodei_save_discount_value/models/sale_order.py
+++ hodei_save_discount_value/models/sale_order.py
@@ -5,10 +5,10 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.onchange('product_id')
-    def _onchange_discount_product(self):
-        super(SaleOrderLine, self).product_uom_change()
-        self.discount = 0
+    # @api.onchange('product_id')
+    # def _onchange_discount_product(self):
+    #     self.discount = 0
+    #     super(SaleOrderLine, self)._onchange_discount()
 
     @api.onchange('price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self):
