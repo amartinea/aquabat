3c6fa42e48f9dee5132c14182d9eadbb525cf07f
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-01-18 11:14:24 +0100

    [FIX] Not save the discount on product change

--- hodei_save_discount_value/models/sale_order.py
+++ hodei_save_discount_value/models/sale_order.py
@@ -5,11 +5,15 @@ from odoo import api, fields, models, _
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
+    @api.onchange('price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self):
         discount = self.discount
         super(SaleOrderLine, self)._onchange_discount()
         self.discount = discount
+
+    @api.onchange('product_id')
+    def _onchange_discount_product(self):
+        super(SaleOrderLine, self)._onchange_discount()
 
     @api.onchange('product_uom', 'product_uom_qty')
     def product_uom_change(self):
