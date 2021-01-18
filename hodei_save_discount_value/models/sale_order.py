33a77ddb563dc3f16ce517b604b74ed021ee2c5d
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-01-18 16:34:56 +0100

    [WIP]

--- hodei_save_discount_value/models/sale_order.py
+++ hodei_save_discount_value/models/sale_order.py
@@ -8,10 +8,22 @@ class SaleOrderLine(models.Model):
 
     @api.onchange('product_id')
     def _onchange_discount_product(self):
+        _logger.info(self.product_id)
+        _logger.info("product")
+        _logger.info(self.product_uom_qty)
+        _logger.info("qty")
+        _logger.info(self.price_unit)
+        _logger.info("price_unit")
         super(SaleOrderLine, self)._onchange_discount()
 
     @api.onchange('price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self):
+        _logger.info(self.product_id)
+        _logger.info("product")
+        _logger.info(self.product_uom_qty)
+        _logger.info("qty")
+        _logger.info(self.price_unit)
+        _logger.info("price_unit")
         super(SaleOrderLine, self)._onchange_discount()
 
 
