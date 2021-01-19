7abebbae98e177f288bc830fb5c02de69b637f97
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-01-19 14:30:29 +0100

    [ADD] Commentary to explain this dirty system

--- hodei_save_discount_value/models/sale_order.py
+++ hodei_save_discount_value/models/sale_order.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
-import logging
-_logger = logging.getLogger(__name__)
+
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
@@ -10,16 +9,30 @@ class SaleOrderLine(models.Model):
 
     @api.onchange('product_id', 'price_unit', 'product_uom', 'product_uom_qty', 'tax_id')
     def _onchange_discount(self):
-        _logger.info("non")
-        _logger.info(self.discount)
-        _logger.info(self.product_id_check)
+        """
+        This function will not be easy to bring to next Odoo version
+
+        Here to fix the discount value after a user modification
+        Update it only when the product_id changed
+
+        The way to do that is dirty :
+         - Use a product_id_check as flag
+         - The flag is set to True after the first time within onchange
+         - The flag is reset at every new user action (dirty/strange)
+         - As long as the flag is False, the discount value is kept
+         - Discount is update when the flag is True
+
+        This system work because this method is triggered many time only when the product value is updated
+        """
         if self.product_id_check != 'check':
             discount = self.discount
+
         super(SaleOrderLine, self)._onchange_discount()
+
         if self.product_id_check != 'check':
             self.discount = discount
         self.product_id_check = 'check'
-        _logger.info(self.discount)
+
 
 
     @api.onchange('product_uom', 'product_uom_qty')
