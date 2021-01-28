41cf3b2b7af47a21093e5972d807f4f6ce807388
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-01-28 16:30:15 +0100

    [FIX] Missing fields

--- hodei_link_invoice_picking/models/stock_picking.py
+++ hodei_link_invoice_picking/models/stock_picking.py
@@ -6,4 +6,4 @@ from odoo import api, fields, models, _
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    invoice_id = Many2one('account.invoice', string='Invoice', default=False)
+    invoice_id = fields.Many2one('account.invoice', string='Invoice', default=False)
