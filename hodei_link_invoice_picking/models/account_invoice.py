7abcbc4a9064034b50df05d119be14c810ec4509
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-01-29 12:26:20 +0100

    [WIP]

--- hodei_link_invoice_picking/models/account_invoice.py
+++ hodei_link_invoice_picking/models/account_invoice.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools.misc import formatLang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+
+
+from odoo.addons import decimal_precision as dp
+
+from werkzeug.urls import url_encode
 import logging
 _logger = logging.getLogger(__name__)
 
