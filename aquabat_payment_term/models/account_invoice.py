9d4b5da70027cb798b2f54c31b9b328bdbac7682
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-10-25 11:23:25 +0200

    [FIX] Wrong indent

--- aquabat_payment_term/models/account_invoice.py
+++ aquabat_payment_term/models/account_invoice.py
@@ -6,8 +6,8 @@ from odoo import fields, models
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-	payment_term_id = fields.Many2one('account.payment.term', string='Payment Terms', oldname='payment_term',
-        readonly=True, states={'draft': [('readonly', False)]},
-        help="If you use payment terms, the due date will be computed automatically at the generation "
-             "of accounting entries. If you keep the payment terms and the due date empty, it means direct payment. "
-             "The payment terms may compute several due dates, for example 50% now, 50% in one month.", required=True)
+	payment_term_id = fields.Many2one('account.payment.term', string='Payment Terms', oldname='payment_term', 
+		readonly=True, states={'draft': [('readonly', False)]}, 
+		help="If you use payment terms, the due date will be computed automatically at the generation " 
+		"of accounting entries. If you keep the payment terms and the due date empty, it means direct payment. " 
+		"The payment terms may compute several due dates, for example 50% now, 50% in one month.", required=True)
--- aquabat_payment_term/models/sale_order.py
+++ aquabat_payment_term/models/sale_order.py
@@ -6,6 +6,5 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    payment_term_id = fields.Many2one(
-        'account.payment.term', string='Payment Terms', check_company=True,
+    payment_term_id = fields.Many2one('account.payment.term', string='Payment Terms', check_company=True, 
         domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]", required=True)
