ce01114dcddcda1aa9f2ede150b7d15ccf9515cb
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-10-28 12:36:04 +0200

    [ADD] Module to manage multi company and accounting entries

---
+++ hodei_accounting_entry_multi_company/models/account_invoice.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    @api.model
+    def invoice_line_move_line_get(self):
+    	res = super(AccountInvoice, self).invoice_line_move_line_get()
+    	if self.company_id:
+    		for res_entry in res:
+    			res_entry['company_id'] = self.company_id
+        return res
