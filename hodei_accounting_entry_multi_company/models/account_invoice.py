e676aa55ee0b8220820205549a497b0bfcefcfb5
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-10-28 13:17:47 +0200

    [WIP] Logs

--- hodei_accounting_entry_multi_company/models/account_invoice.py
+++ hodei_accounting_entry_multi_company/models/account_invoice.py
@@ -2,13 +2,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
+
     @api.model
     def finalize_invoice_move_lines(self, line):
-    	console.log('---------------------ici----------------')
-    	console.log(line)
+        _logger.warning('--------------------ici-------------')
+        _logger.warning(line)
         return line
