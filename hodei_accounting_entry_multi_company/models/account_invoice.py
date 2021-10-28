3c0e099fbe59dfa397a59394d36d61482e3fe85f
Arthur Martineau <arthur.martineau@meta-it.fr>
2021-10-28 12:41:27 +0200

    [FIX] Wrong indent

--- hodei_accounting_entry_multi_company/models/account_invoice.py
+++ hodei_accounting_entry_multi_company/models/account_invoice.py
@@ -9,8 +9,8 @@ class AccountInvoice(models.Model):
 
     @api.model
     def invoice_line_move_line_get(self):
-    	res = super(AccountInvoice, self).invoice_line_move_line_get()
-    	if self.company_id:
-    		for res_entry in res:
-    			res_entry['company_id'] = self.company_id
+        res = super(AccountInvoice, self).invoice_line_move_line_get()
+        if self.company_id:
+            for res_entry in res:
+                res_entry['company_id'] = self.company_id
         return res
