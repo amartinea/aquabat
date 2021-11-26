abd20e30d66fbd6ccf5f170d8f1dfda361b9178b
AM <a.martineau@hodei.net>
2021-11-26 08:27:36 +0100

    [ADD] Module to link account_payment_term between two company

---
+++ hodei_link_payment_term/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Hodei link between same payment term for different company module',
+    'version': '1.0.0',
+    'category': '',
+    'description': """ """,
+    'depends': [
+        'account',
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'views/account_payment_views.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
