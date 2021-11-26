f04de6d444b45cf1544c64798090b7c32b7ab735
AM <a.martineau@hodei.net>
2021-11-26 10:38:35 +0100

    [FIX]

--- hodei_link_payment_term/models/account_payment.py
+++ hodei_link_payment_term/models/account_payment.py
@@ -6,4 +6,4 @@ from odoo import fields, models
 class AccountPaymentTerm(models.Model):
     _inherit = "account.payment.term"
 
-    link_payment_term_id = fields.Many2one('account.payment.term', string='Payment link for the other company', domain=[('company_id', '!=', user.company_id.id)], default=False)
+    link_payment_term_id = fields.Many2one('account.payment.term', string='Payment link for the other company', default=False)
