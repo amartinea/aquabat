ac69a54429741358bb987cfd447f192a9408b41e
AM <a.martineau@hodei.net>
2021-11-29 07:45:41 +0100

    [ADD] Logs

--- aquabat_link_payment_term/models/res_partner.py
+++ aquabat_link_payment_term/models/res_partner.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
@@ -9,7 +11,10 @@ class ResPartner(models.Model):
     @api.onchange('property_payment_term_id')
     def _onchange_property_payment_term_id(self):
         property_to_copy = self.property_payment_term_id.link_payment_term_id
+        _logger.warning(property_to_copy)
         if self.company_id == 1:
+            _logger.warning('__________________________1')
             self.with_context(force_company=3).write({'property_payment_term_id': property_to_copy})
         elif self.company_id == 3:
+            _logger.warning('__________________________3')
             self.with_context(force_company=1).write({'property_payment_term_id': property_to_copy})
