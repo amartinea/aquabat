6afaf57cd1e5e636b6626fcd7a51d703f2616aa8
AM <a.martineau@hodei.net>
2021-11-29 07:37:28 +0100

    [FIX] Way to get current company_id

--- aquabat_link_payment_term/models/res_partner.py
+++ aquabat_link_payment_term/models/res_partner.py
@@ -9,7 +9,7 @@ class ResPartner(models.Model):
     @api.onchange('property_payment_term_id')
     def _onchange_property_payment_term_id(self):
         property_to_copy = self.property_payment_term_id.link_payment_term_id
-        if self.env.company.id == 1:
+        if self.company_id == 1:
             self.with_context(force_company=3).write({'property_payment_term_id': property_to_copy})
-        elif self.env.company.id == 3:
+        elif self.company_id == 3:
             self.with_context(force_company=1).write({'property_payment_term_id': property_to_copy})
