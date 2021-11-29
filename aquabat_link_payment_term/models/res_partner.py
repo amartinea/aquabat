8255be7d3983b519a593af2aa8d810d7f31d788d
AM <a.martineau@hodei.net>
2021-11-29 07:20:12 +0100

    [ADD] Module to auto link payment term

---
+++ aquabat_link_payment_term/models/res_partner.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.onchange('property_payment_term_id')
+    def _onchange_property_payment_term_id(self):
+        property_to_copy = self.property_payment_term_id.link_payment_term_id
+        if self.env.company.id == 1:
+            self.with_context(force_company=3).write({'property_payment_term_id': property_to_copy})
+        elif self.env.company.id == 3:
+            self.with_context(force_company=1).write({'property_payment_term_id': property_to_copy})
