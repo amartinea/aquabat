9b2466135ae9cc82160e51966e2fce49c9a33b2d
AM <a.martineau@hodei.net>
2021-11-29 07:48:00 +0100

    [FIX] Wrong test

--- aquabat_link_payment_term/models/res_partner.py
+++ aquabat_link_payment_term/models/res_partner.py
@@ -12,9 +12,9 @@ class ResPartner(models.Model):
     def _onchange_property_payment_term_id(self):
         property_to_copy = self.property_payment_term_id.link_payment_term_id
         _logger.warning(property_to_copy)
-        if self.company_id == 1:
+        if self.company_id.id == 1:
             _logger.warning('__________________________1')
             self.with_context(force_company=3).write({'property_payment_term_id': property_to_copy})
-        elif self.company_id == 3:
+        elif self.company_id.id == 3:
             _logger.warning('__________________________3')
             self.with_context(force_company=1).write({'property_payment_term_id': property_to_copy})
