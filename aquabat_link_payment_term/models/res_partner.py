5d344635ab2d866a91c87b12cc3a765ef0f7c945
AM <a.martineau@hodei.net>
2021-11-30 07:41:55 +0100

    [WIP]

--- aquabat_link_payment_term/models/res_partner.py
+++ aquabat_link_payment_term/models/res_partner.py
@@ -8,15 +8,15 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    def write(self, vals):
-        if 'property_payment_term_id' in vals and vals['property_payment_term_id']:
-            property_to_copy = self.property_payment_term_id.link_payment_term_id
+    def create(self, vals):
+        new_partner = super(ResPartner, self).create(vals)
+        _logger.warning(new_partner)
+        if new_partner['property_payment_term_id']:
+            property_to_copy = new_partner.property_payment_term_id.link_payment_term_id
             _logger.warning(property_to_copy)
-            _logger.warning(self.company_id)
-            _logger.info(vals.company_id)
-            if self.company_id.id == 1:
+            if new_partner.company_id.id == 1:
                 _logger.warning('__________________________1')
-                self.with_context(force_company=3).write({'property_payment_term_id': property_to_copy})
-            elif self.company_id.id == 3:
+                new_partner.with_context(force_company=3).write({'property_payment_term_id': property_to_copy})
+            elif new_partner.company_id.id == 3:
                 _logger.warning('__________________________3')
-                self.with_context(force_company=1).write({'property_payment_term_id': property_to_copy})
+                new_partner.with_context(force_company=1).write({'property_payment_term_id': property_to_copy})
