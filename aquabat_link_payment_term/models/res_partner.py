a6c166455085a8f5467a5359921f86dd1a51c5aa
AM <a.martineau@hodei.net>
2021-11-30 07:34:06 +0100

    [WIP]

--- aquabat_link_payment_term/models/res_partner.py
+++ aquabat_link_payment_term/models/res_partner.py
@@ -8,13 +8,15 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    @api.depends('property_payment_term_id')
-    def _onchange_property_payment_term_id(self):
-        property_to_copy = self.property_payment_term_id.link_payment_term_id
-        _logger.warning(property_to_copy)
-        if self.company_id.id == 1:
-            _logger.warning('__________________________1')
-            self.with_context(force_company=3).write({'property_payment_term_id': property_to_copy})
-        elif self.company_id.id == 3:
-            _logger.warning('__________________________3')
-            self.with_context(force_company=1).write({'property_payment_term_id': property_to_copy})
+    def write(self, vals):
+        if 'property_payment_term_id' in vals and vals['property_payment_term_id']:
+            property_to_copy = self.property_payment_term_id.link_payment_term_id
+            _logger.warning(property_to_copy)
+            _logger.warning(self.company_id)
+            _logger.info(vals.company_id)
+            if self.company_id.id == 1:
+                _logger.warning('__________________________1')
+                self.with_context(force_company=3).write({'property_payment_term_id': property_to_copy})
+            elif self.company_id.id == 3:
+                _logger.warning('__________________________3')
+                self.with_context(force_company=1).write({'property_payment_term_id': property_to_copy})
