efad56857a14cce0d6bece05cf27332eaf91a70a
Arthur Martineau <amartineau@teclib.com>
2021-12-20 10:48:46 +0100

    [WIP]

--- aquabat_link_payment_term/models/res_partner.py
+++ aquabat_link_payment_term/models/res_partner.py
@@ -18,12 +18,10 @@ class ResPartner(models.Model):
         if new_partner['property_payment_term_id']:
             _logger.warning('if')
             property_to_copy = new_partner.property_payment_term_id.link_payment_term_id
-            _logger.warning(property_to_copy)
-            _logger.debug(new_partner['company_id'])
-            _logger.debug(new_partner.company_id)
+            _logger.warning(property_to_copy['id'])
             if new_partner.company_id.id == 1:
                 _logger.warning('__________________________1')
-                new_partner.with_context(force_company=3).write({'property_payment_term_id': property_to_copy.id})
+                new_partner.with_context(force_company=3).write({'property_payment_term_id': property_to_copy['id']})
             elif new_partner.company_id.id == 3:
                 _logger.warning('__________________________3')
-                new_partner.with_context(force_company=1).write({'property_payment_term_id': property_to_copy.id})
+                new_partner.with_context(force_company=1).write({'property_payment_term_id': property_to_copy['id'] })
