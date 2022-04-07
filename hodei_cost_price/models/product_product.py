67bd84b291a7cbeaf421fb613bc9e5d82576595b
Arthur Martineau <amartineau@teclib.com>
2022-04-07 16:48:28 +0200

    [FIX] Use company_dependent

--- hodei_cost_price/models/product_product.py
+++ hodei_cost_price/models/product_product.py
@@ -6,7 +6,7 @@ from odoo import api, fields, models, _
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    cost_price = fields.Float('Cost price', compute="_compute_cost_price", store=True)
+    cost_price = fields.Float('Cost price', company_dependent=True, store=True)
     coef_item_ids = fields.Many2many('product.coeflist.item', 'Coeflist Items', compute='_get_coeflist_items')
 
     @api.one
@@ -16,11 +16,11 @@ class ProductProduct(models.Model):
             ('product_id', '=', self.id),
             ('product_tmpl_id', '=', self.product_tmpl_id.id)]).ids
 
-    @api.depends('coef_item_ids', 'standard_price')
-    def _compute_cost_price(self):        
+    @api.onchange('coef_item_ids', 'standard_price')
+    def onchange_cost_price(self):
         for product in self:
             coeflist_items = self.env['product.coeflist.item'].search([
-                '|','|',('categ_id', '=', product.categ_id.id),
+                '|', '|', ('categ_id', '=', product.categ_id.id),
                 ('product_id', '=', product.id),
                 ('product_tmpl_id', '=', product.product_tmpl_id.id)])
             coef_categ = 0
@@ -35,19 +35,58 @@ class ProductProduct(models.Model):
                 coef = coef_categ
             if coef_product != 0:
                 coef = coef_product
-            product.cost_price = product.standard_price * coef
+            product.with_context(force_company=self.env.company.id).cost_price = product.with_context(
+                force_company=self.env.company.id).standard_price * coef
             if product.product_tmpl_id:   #Not exist when create product
-                product.product_tmpl_id.cost_price = product.standard_price * coef
+                product.product_tmpl_id.with_context(
+                    force_company=self.env.company.id).cost_price = product.with_context(
+                    force_company=self.env.company.id).standard_price * coef
+
+    #
+    #
+    # @api.depends('coef_item_ids', 'standard_price')
+    # def _compute_cost_price(self):
+    #     for product in self:
+    #         coeflist_items = self.env['product.coeflist.item'].search([
+    #             '|','|',('categ_id', '=', product.categ_id.id),
+    #             ('product_id', '=', product.id),
+    #             ('product_tmpl_id', '=', product.product_tmpl_id.id)])
+    #         coef_categ = 0
+    #         coef_product = 0
+    #         coef = 1
+    #         for coeflist_item in coeflist_items:
+    #             if coeflist_item.product_tmpl_id:
+    #                 coef_product = coeflist_item.coef_value
+    #             if coeflist_item.categ_id:
+    #                 coef_categ = coeflist_item.coef_value
+    #         if coef_categ != 0:
+    #             coef = coef_categ
+    #         if coef_product != 0:
+    #             coef = coef_product
+    #         product.cost_price = product.standard_price * coef
+    #         if product.product_tmpl_id:   #Not exist when create product
+    #             product.product_tmpl_id.cost_price = product.standard_price * coef
+
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    cost_price = fields.Float('Cost price', compute="_compute_cost_price", store=True)
+    cost_price = fields.Float('Cost price', company_dependent=True, store=True)
 
-    @api.depends('product_variant_ids', 'product_variant_ids.standard_price')
-    def _compute_cost_price(self):
+    @api.onchange('product_variant_ids', 'product_variant_ids.standard_price')
+    def onchange_cost_price(self):
         unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
         for template in unique_variants:
-            template.cost_price = template.product_variant_ids.cost_price
+            template.with_context(
+                force_company=self.env.company.id).cost_price = template.product_variant_ids.with_context(
+                force_company=self.env.company.id).cost_price
         for template in (self - unique_variants):
-            template.cost_price = 0.0
+            template.with_context(force_company=self.env.company.id).cost_price = 0.0
+    #
+    # @api.depends('product_variant_ids', 'product_variant_ids.standard_price')
+    # def _compute_cost_price(self):
+    #     unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+    #     for template in unique_variants:
+    #         template.cost_price = template.product_variant_ids.cost_price
+    #     for template in (self - unique_variants):
+    #         template.cost_price = 0.0
