ed215d4d4e60ff0b594aa2b97fb7466d083c2c09
Arthur Martineau <amartineau@teclib.com>
2022-04-11 00:15:12 +0200

    [FIX] Clean code

--- hodei_cost_price/models/product_product.py
+++ hodei_cost_price/models/product_product.py
@@ -42,31 +42,6 @@ class ProductProduct(models.Model):
                     force_company=self.env.user.company_id.id).cost_price = product.with_context(
                     force_company=self.env.user.company_id.id).standard_price * coef
 
-    #
-    #
-    # @api.depends('coef_item_ids', 'standard_price')
-    # def _compute_cost_price(self):
-    #     for product in self:
-    #         coeflist_items = self.env['product.coeflist.item'].search([
-    #             '|','|',('categ_id', '=', product.categ_id.id),
-    #             ('product_id', '=', product.id),
-    #             ('product_tmpl_id', '=', product.product_tmpl_id.id)])
-    #         coef_categ = 0
-    #         coef_product = 0
-    #         coef = 1
-    #         for coeflist_item in coeflist_items:
-    #             if coeflist_item.product_tmpl_id:
-    #                 coef_product = coeflist_item.coef_value
-    #             if coeflist_item.categ_id:
-    #                 coef_categ = coeflist_item.coef_value
-    #         if coef_categ != 0:
-    #             coef = coef_categ
-    #         if coef_product != 0:
-    #             coef = coef_product
-    #         product.cost_price = product.standard_price * coef
-    #         if product.product_tmpl_id:   #Not exist when create product
-    #             product.product_tmpl_id.cost_price = product.standard_price * coef
-
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
@@ -82,11 +57,3 @@ class ProductTemplate(models.Model):
                 force_company=self.env.user.company_id.id).cost_price
         for template in (self - unique_variants):
             template.with_context(force_company=self.env.user.company_id.id).cost_price = 0.0
-    #
-    # @api.depends('product_variant_ids', 'product_variant_ids.standard_price')
-    # def _compute_cost_price(self):
-    #     unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
-    #     for template in unique_variants:
-    #         template.cost_price = template.product_variant_ids.cost_price
-    #     for template in (self - unique_variants):
-    #         template.cost_price = 0.0
