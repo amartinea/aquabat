7864300dbe93e535ff95985de9c6b552d74d9ce7
Arthur Martineau <amartineau@teclib.com>
2022-04-12 01:02:21 +0200

    [WIP]

--- hodei_cost_price/models/product_product.py
+++ hodei_cost_price/models/product_product.py
@@ -18,7 +18,7 @@ class ProductProduct(models.Model):
             ('product_id', '=', self.id),
             ('product_tmpl_id', '=', self.product_tmpl_id.id)]).ids
 
-    @api.onchange('coef_item_ids', 'standard_price')
+    @api.onchange('standard_price')
     def onchange_cost_price(self):
         _logger.warning('onchange______________')
         for product in self:
