0cc01f0e0837a45fe9a874ab99a9ac085e854f25
Arthur Martineau <amartineau@teclib.com>
2022-08-24 17:36:34 +0200

    [ADD] Create update

--- hodei_link_payment_term/models/res_partner.py
+++ hodei_link_payment_term/models/res_partner.py
@@ -7,6 +7,38 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
+    def create(self, vals):
+        res = super(ResPartner, self).create(vals)
+        _logger.warning('res :')
+        _logger.warning(res)
+        _logger.warning('vals :')
+        _logger.warning(vals)
+        company_id = self.env.user.company_id.id
+        o_company = self.env['res.company'].search([('id', '!=', company_id)])[0]
+        res_id = 'res.partner,' + str(self.id)
+        if vals.get('property_payment_term_id'):
+            _logger.warning('oui')
+            term = self.env['account.payment.term'].search([('id', '=', vals['property_payment_term_id'])])
+            self.env['ir.property'].create({
+                'name': 'property_payment_term_id, test',
+                'company_id': o_company['id'],
+                'res_id': res_id,
+                'fields_id': 2435,
+                'value_reference': 'account.payment.term' + str(term['link_payment_term_id']['id']),
+                'type': 'many2one'
+            })
+        if vals.get('property_supplier_payment_term_id'):
+            _logger.warning('oui')
+            term = self.env['account.payment.term'].search([('id', '=', vals['property_supplier_payment_term_id'])])
+            self.env['ir.property'].write({
+                'name': 'property_supplier_payment_term_id, test',
+                'company_id': company_id,
+                'res_id': res_id,
+                'fields_id': 2422,
+                'value_reference': 'account.payment.term' + str(term['link_payment_term_id']['id']),
+                'type': 'many2one'
+            })
+        return res
     def write(self, values):
         res = super(ResPartner, self).write(values)
         company_id = self.env.user.company_id.id
