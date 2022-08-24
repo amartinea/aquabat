a6b1343e5478b9cabf1a7c57856507f782961953
Arthur Martineau <amartineau@teclib.com>
2022-08-24 16:26:13 +0200

    [FIX] Missing super

--- hodei_link_payment_term/models/res_partner.py
+++ hodei_link_payment_term/models/res_partner.py
@@ -7,19 +7,19 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     def write(self, values):
+        res = super(ResPartner, self).write(values)
         company_id = self.env.user.company_id.id
         res_id = 'res.partner,' + self.id
-        # if values.get('property_payment_term_id'):
-        #     fields_id = id
-        #     value_reference = 'account.payment.termupdate_cost_price,' + id
-        #     # self.env['ir.property'].write({
-        #     #     'name': 'property_payment_term_id',
-        #     #     'company_id':,
-        #     #     'res_id':,
-        #     #     'fields_id: ,
-        #     #     'value_reference': ,
-        #     #     'type': 'many2one'
-        #     # })
+        if values.get('property_payment_term_id'):
+            term = self.env['account.payment.term'].search([('id', '=', values['property_payment_term_id'])])
+            self.env['ir.property'].write({
+                'name': 'property_payment_term_id',
+                'company_id': company_id,
+                'res_id': res_id,
+                'fields_id': 2435,
+                'value_reference': 'account.payment.term' + term['link_payment_term_id']['id'],
+                'type': 'many2one'
+            })
         if values.get('property_supplier_payment_term_id'):
             term = self.env['account.payment.term'].search([('id', '=', values['property_supplier_payment_term_id'])])
             self.env['ir.property'].write({
