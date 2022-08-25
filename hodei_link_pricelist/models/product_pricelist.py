9dae057f6541c5861c0229003e6a2d6c6b97bccd
Arthur Martineau <amartineau@teclib.com>
2022-08-25 11:27:08 +0200

    [FIX] For price_list, use the same price_list for both company

--- hodei_link_pricelist/models/product_pricelist.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class ProductPricelist(models.Model):
-    _inherit = "product.pricelist"
-
-    link_product_pricelist_id = fields.Many2one('product.pricelist', string='Product Pricelist for the other company', default=False)
