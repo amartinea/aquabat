db504767927798f6003d2ef1ad71fda44352abd9
Arthur Martineau <amartineau@teclib.com>
2022-08-26 12:15:05 +0200

    [FIX] Use append to add value on list

--- hodei_link_taxes/models/product_template.py
+++ hodei_link_taxes/models/product_template.py
@@ -12,8 +12,9 @@ class ProductTemplate(models.Model):
         _logger.warning(vals_list)
         for vals in vals_list:
             _logger.warning(vals['taxes_id'])
-            _logger.warning(vals['taxes_id'][0])
+            _logger.warning(vals['taxes_id'][0][2])
             if 'taxes_id' in vals and len(vals['taxes_id'][0][2]) == 1:
-                vals['taxes_id'][0][2] += self.env['account.tax'].search([('id', '=', vals['taxes_id'][0][2])])['link_tax_id']['id']
+                vals['taxes_id'][0][2].append(
+                    self.env['account.tax'].search([('id', '=', vals['taxes_id'][0][2])])['link_tax_id']['id'])
             _logger.warning(vals)
         return super(ProductTemplate, self).create(vals_list)
