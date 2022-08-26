fa8f355797955310b0c7adc34700b657ca97a2cb
Arthur Martineau <amartineau@teclib.com>
2022-08-26 12:08:50 +0200

    [FIX] vals is a list

--- hodei_link_taxes/models/product_template.py
+++ hodei_link_taxes/models/product_template.py
@@ -8,11 +8,12 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     @api.model_create_multi
-    def create(self, vals):
-        _logger.warning(vals)
-        _logger.warning(vals['taxes_id'])
-        _logger.warning(vals['taxes_id'][0])
-        if 'taxes_id' in vals and len(vals['taxes_id'][0][2]) == 1:
-            vals['taxes_id'][0][2] += self.env['account.tax'].search([('id', '!=',vals['taxes_id'][0][2])])['link_tax_id']['id']
-        _logger.warning(vals)
-        return super(ProductTemplate, self).create(vals)
+    def create(self, vals_list):
+        _logger.warning(vals_list)
+        for vals in vals_list:
+            _logger.warning(vals['taxes_id'])
+            _logger.warning(vals['taxes_id'][0])
+            if 'taxes_id' in vals and len(vals['taxes_id'][0][2]) == 1:
+                vals['taxes_id'][0][2] += self.env['account.tax'].search([('id', '!=',vals['taxes_id'][0][2])])['link_tax_id']['id']
+            _logger.warning(vals)
+        return super(ProductTemplate, self).create(vals_list)
