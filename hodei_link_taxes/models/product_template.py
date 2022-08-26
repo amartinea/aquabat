c6c6ee3a1a6a25b6fa212728c92eb52b394288b1
Arthur Martineau <amartineau@teclib.com>
2022-08-26 11:59:23 +0200

    [FIX] Way to insert new taxes

--- hodei_link_taxes/models/product_template.py
+++ hodei_link_taxes/models/product_template.py
@@ -9,8 +9,8 @@ class ProductTemplate(models.Model):
 
     @api.model_create_multi
     def create(self, vals):
-        _logger.warning(vals)
-        if 'taxes_id' in vals and len(vals['taxes_id']) == 1:
-            vals['taxes_id'] += self.env['account.tax'].search([('id', '!=', vals['taxes_id'])])['link_tax_id']['id']
+        _logger.warning(vals['taxes_id'][0][2])
+        if 'taxes_id' in vals and len(vals['taxes_id'][0[2]]) == 1:
+            vals['taxes_id'][0][2] += self.env['account.tax'].search([('id', '!=',vals['taxes_id'][0][2])])['link_tax_id']['id']
         _logger.warning(vals)
         return super(ProductTemplate, self).create(vals)
